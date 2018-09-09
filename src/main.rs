85ffea5ec340bd1b0b0ee9f77f1934633853b00c
Keith Smiley <keithbsmiley@gmail.com>
2018-09-09 14:37:15 -0700

    Handle rename lines (#2)
    This has a few changes:
    
    - Handle renamed lines, which happens when you stage changes, to only print the new filename. Ex: ` R foo.txt -> bar.txt` will print `bar.txt`. Note files with ` -> ` in their names are not supported and will cause an assertion failure
    - Fix quote handling, previously files like ` M "foo bar.txt"` would be incorrectly quoted if they were relative paths, now the quotes are stripped, and quotes are re-added to all files before printing. Note that files with quotes in the names are not supported

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,95 @@ fn run_git_command(args: &[&str]) -> Option<String> {
     return Some(String::from(stdout));
 }
 
+fn new_file_from_rename(line: &str) -> String {
+    let files: Vec<&str> = line.split(" -> ").collect();
+    assert!(files.len() == 2, "Had file with '->' in the name");
+    return files.last().unwrap().to_string();
+}
+
+#[test]
+fn test_simple_rename() {
+    assert_eq!(new_file_from_rename("foo.txt -> bar.txt"), "bar.txt");
+}
+
+#[test]
+#[should_panic(expected = "Had file with '->' in the name")]
+fn test_complex_rename() {
+    new_file_from_rename("\"foo -> bar.txt\" -> baz.txt");
+}
+
+fn file_path_for_line(line: &str) -> Option<String> {
+    let trimmed = line.trim();
+    let idx = trimmed
+        .find(char::is_whitespace)
+        .expect("No space in line, unexpected output from git status");
+    let (file_status, file) = trimmed.split_at(idx);
+
+    match file_status {
+        "D" => None,
+        "R" | "C" => Some(new_file_from_rename(&file.trim())),
+        _ => Some(file.trim().to_string()),
+    }.map(|x| x.trim_matches('"').to_string())
+}
+
+#[test]
+fn test_path_for_modified_line() {
+    assert_eq!(
+        file_path_for_line(" M foo.txt"),
+        Some("foo.txt".to_string())
+    );
+}
+
+#[test]
+fn test_path_for_renamed_line() {
+    assert_eq!(
+        file_path_for_line(" R foo.txt -> bar.txt"),
+        Some("bar.txt".to_string())
+    );
+}
+
+#[test]
+fn test_path_for_changed_line() {
+    assert_eq!(
+        file_path_for_line(" C foo.txt -> bar.txt"),
+        Some("bar.txt".to_string())
+    );
+}
+
+#[test]
+fn test_path_for_deleted_line() {
+    assert_eq!(file_path_for_line(" D foo.txt"), None);
+}
+
+fn paths_for_lines<'a, I>(lines: I) -> Vec<std::path::PathBuf>
+where
+    I: Iterator<Item = &'a str>,
+{
+    lines
+        .into_iter()
+        .filter_map(file_path_for_line)
+        .map(std::path::PathBuf::from)
+        .collect()
+}
+
+#[test]
+fn test_getting_paths_from_lines() {
+    let lines = vec![
+        " M foo.txt",
+        " M \"bar baz.txt\"",
+        " R qux.txt -> quxx.txt",
+        " D deleted.txt",
+    ];
+
+    let expected: Vec<std::path::PathBuf> =
+        ["foo.txt", "bar baz.txt", "quxx.txt"]
+            .iter()
+            .map(std::path::PathBuf::from)
+            .collect();
+
+    assert_eq!(paths_for_lines(lines.into_iter()), expected)
+}
+
 fn main() {
     let git_dir = match run_git_command(&["rev-parse", "--show-toplevel"]) {
         Some(output) => std::path::PathBuf::from(&output),
@@ -39,17 +128,9 @@ fn main() {
     let path_from_root = pathdiff::diff_paths(&pwd, &git_dir)
         .expect("Both paths should be absolute");
 
-    for line in status.lines().map(|x| x.trim()) {
-        let idx = line
-            .find(" ")
-            .expect("No space in line, unexpected output from git status");
-        let (file_status, file) = line.split_at(idx);
-        if file_status != "D" {
-            let file_path = std::path::Path::new(file.trim());
-            let relative_path =
-                pathdiff::diff_paths(file_path, &path_from_root)
-                    .expect("File must be in git repo");
-            println!("{}", relative_path.display());
-        }
+    for file_path in paths_for_lines(status.lines()) {
+        let relative_path = pathdiff::diff_paths(&file_path, &path_from_root)
+            .expect("File must be in git repo");
+        println!("\"{}\"", relative_path.display());
     }
 }
